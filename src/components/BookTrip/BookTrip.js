555ed17879310ab3a94fa78914954d01343cde16
jgabaja <jgabaja@gmail.com>
2018-12-01 17:09:02 +0800

    wip

--- src/components/BookTrip/BookTrip.js
+++ src/components/BookTrip/BookTrip.js
@@ -1,12 +1,53 @@
 import React, {Component} from 'react';
 import TabsPassenger from '../TabsPassenger/TabsPassenger';
+import axios from 'axios';
+import {Table, Button} from 'reactstrap';
 
 class BookTrip extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      trips: []
+    };
+  }
+
+  componentWillMount() {
+    axios.get('https://tickets-backend.herokuapp.com/trips/')
+    .then(response => {
+      this.setState({trips: response.data});
+    });
+  }
+
   render() {
     return(
       <div className='container'>
         <TabsPassenger active={'book'} />
         <h4>Book a Trip</h4>
+        <Table>
+          <thead>
+            <tr>
+              <th>Departure Date</th>
+              <th>Departure Time</th>
+              <th>Source Terminal</th>
+              <th>Destination Terminal</th>
+              <th>Seats Left</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+          {this.state.trips.length > 0 && this.state.trips.map((item, i) => {
+            return(<tr>
+              <td>{item.departureDate}</td>
+              <td>{item.departureTime}</td>
+              <td>{item.sourceTerminal}</td>
+              <td>{item.destinationTerminal}</td>
+              <td>{item.seatsLeft}</td>
+              <td><Button color='success'>Book Trip</Button></td>
+            </tr>);
+          })} 
+          </tbody>
+        </Table> 
+        
       </div>
     );
   }
--- src/components/HomePassenger/HomePassenger.js
+++ src/components/HomePassenger/HomePassenger.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Nav, NavItem, Dropdown, DropdownToggle, DropdownMenu, DropdownItem, NavLink} from 'reactstrap';
+import {Nav, NavItem, Dropdown, DropdownToggle, DropdownMenu, DropdownItem, NavLink, Button} from 'reactstrap';
 import TabsPassenger from '../TabsPassenger/TabsPassenger';
 
 class HomePassenger extends Component {
@@ -14,11 +14,18 @@ class HomePassenger extends Component {
     this.setState({tripsDD: !this.state.tripsDD});
   }
 
+  logOut = () => {
+    localStorage.removeItem('displayName');
+    localStorage.removeItem('username');
+    this.props.history.push('/');
+  }
+
   render() {
     return(
       <div className='container'>
         <TabsPassenger active={'dashboard'} />
         <h4>Welcome back, {localStorage.getItem('displayName')}.</h4>
+        <Button onClick={this.logOut} color='warning' outline>Log out</Button>
       </div>
     );
   }
